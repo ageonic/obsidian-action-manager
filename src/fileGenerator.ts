ec9d749a0f8cca2fbcd320355c8f9cbba228d76d
Aaron George <79060613+ageonic@users.noreply.github.com>
2022-12-18 00:10:30 -0500

    feat: add base structure for new class to assist with file generation

---
+++ src/fileGenerator.ts
@@ -0,0 +1,9 @@
+import ActionManager from "main";
+
+export default class FileGenerator {
+    plugin: ActionManager;
+
+    constructor(plugin: ActionManager) {
+        this.plugin = plugin;
+    }
+}
