4c37838ab30713a1b238d37ad510d075bd1f455a
Aaron George <79060613+ageonic@users.noreply.github.com>
2022-12-17 23:15:41 -0500

    refactor: update settings structure and tab layout

--- src/settings.ts
+++ src/settings.ts
@@ -3,28 +3,101 @@ import { Action, Meta } from "./core/types";
 import ActionManager from "./main";
 
 export interface ActionManagerSettings {
-    directories: {
-        [Action.Task]: string;
-        [Action.Activity]: string;
-        [Action.Reminder]: string;
-        [Action.FollowUp]: string;
-        [Meta.Organization]: string;
-        [Meta.Individual]: string;
-        [Meta.Project]: string;
+    core: {
+        [Action.Task]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
+        [Action.Activity]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
+        [Action.Reminder]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
+        [Action.FollowUp]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
+        [Meta.Organization]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
+        [Meta.Individual]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
+        [Meta.Project]: {
+            nameFormat: string,
+            defaultLocation: string,
+        },
     }
 }
 
 export const DEFAULT_SETTINGS: Partial<ActionManagerSettings> = {
-    directories: {
-        [Action.Task]: "/",
-        [Action.Activity]: "/",
-        [Action.Reminder]: "/",
-        [Action.FollowUp]: "/",
-        [Meta.Organization]: "/",
-        [Meta.Individual]: "/",
-        [Meta.Project]: "/",
+    core: {
+        [Action.Task]: {
+            nameFormat: "TSK{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
+        [Action.Activity]: {
+            nameFormat: "ACT{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
+        [Action.Reminder]: {
+            nameFormat: "RMD{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
+        [Action.FollowUp]: {
+            nameFormat: "FLP{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
+        [Meta.Organization]: {
+            nameFormat: "ORG{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
+        [Meta.Individual]: {
+            nameFormat: "IND{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
+        [Meta.Project]: {
+            nameFormat: "PRJ{YY}{DD}{00}",
+            defaultLocation: "/",
+        },
     }
 };
+
+const typeDescribe = {
+    [Action.Task]: {
+        label: "Task",
+        pluralLabel: "Tasks"
+    },
+    [Action.Activity]: {
+        label: "Activity",
+        pluralLabel: "Activities"
+    },
+    [Action.Reminder]: {
+        label: "Reminder",
+        pluralLabel: "Reminders"
+    },
+    [Action.FollowUp]: {
+        label: "Follow Up",
+        pluralLabel: "Follow Ups"
+    },
+    [Meta.Organization]: {
+        label: "Organization",
+        pluralLabel: "Organizations"
+    },
+    [Meta.Individual]: {
+        label: "Individual",
+        pluralLabel: "Individuals"
+    },
+    [Meta.Project]: {
+        label: "Project",
+        pluralLabel: "Projects"
+    },
+}
 
 export class ActionManagerSettingsTab extends PluginSettingTab {
     plugin: ActionManager;
@@ -39,17 +112,33 @@ export class ActionManagerSettingsTab extends PluginSettingTab {
 
         containerEl.empty();
 
-        Object.keys(this.plugin.settings.directories).forEach(dirType => {
+        Object.keys(this.plugin.settings.core).forEach(coreType => {
+            const describe = typeDescribe[coreType as Action | Meta];
+            const spec = this.plugin.settings.core[coreType as Action | Meta];
+
+            containerEl.createEl("h2", { text: describe.pluralLabel });
+
             new Setting(containerEl)
-                .setName(dirType)
-                .setDesc("Default location for creating files of type " + dirType)
+                .setName('Naming Format')
+                .setDesc(`This template is used to generate unique IDs for each ${describe.label}. The unique ID is also used as the file name.`)
+                .addText(text => text
+                    .setValue(spec.nameFormat)
+                    .onChange(async (value) => {
+                        spec.nameFormat = value;
+                        await this.plugin.saveSettings();
+                    })
+                );
+
+            new Setting(containerEl)
+                .setName('Location')
+                .setDesc(`The default location for storing ${describe.pluralLabel}.`)
                 .addDropdown(dropdown => {
                     this.app.vault.getAllLoadedFiles().filter((f: TAbstractFile) => f instanceof TFolder).forEach(folder => dropdown.addOption(folder.path, folder.path));
 
                     dropdown
-                        .setValue(this.plugin.settings.directories[dirType])
+                        .setValue(spec.defaultLocation)
                         .onChange(async (value) => {
-                            this.plugin.settings.directories[dirType] = value;
+                            spec.defaultLocation = value;
                             await this.plugin.saveSettings();
                         });
                 });
