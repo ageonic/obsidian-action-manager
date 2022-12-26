9b840fe868ae9fa6210a5dcd20a6b71ab6d43a68
Aaron George <79060613+ageonic@users.noreply.github.com>
2022-12-26 15:07:04 -0500

    feat: add method to build the content string based on specified templates and properties

--- src/file/fileGenerator.ts
+++ src/file/fileGenerator.ts
@@ -1,5 +1,7 @@
+import { Properties, renderFields } from "core/properties";
 import { Action, Meta } from "core/types";
 import ActionManager from "main";
+import FrontMatterParser from "./frontMatterParser";
 
 const moment = require('moment');
 
@@ -23,5 +25,18 @@ export default class FileGenerator {
             .replace('{MM}', moment().format('MM'))
             .replace('{DD}', moment().format('DD'))
             .replace('{00}', serialNumber.toString().padStart(2, '0'));
+    }
+
+    async generateContent(fileType: Action | Meta, props: Properties) {
+        const templatePath = this.plugin.settings.core[fileType].templatePath;
+
+        let parser = new FrontMatterParser();
+
+        if (templatePath)
+            await parser.load(templatePath);
+
+        parser.addAllProperties(renderFields(props));
+
+        return parser.buildContent();
     }
 }
